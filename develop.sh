72098f8146c778f600982e58b77ee196f13a73aa
yingnengd <80525738+yingnengd@users.noreply.github.com>
2023-08-14 23:19:14 +0800

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -12,35 +12,35 @@ medusa migrations run
 #Start development environment
 medusa develop
 
-#add user
-medusa user -e mei@outlook.com -p Syw!888888
+# #add user
+# medusa user -e mei@outlook.com -p Syw!888888
 
-#install pluns
-yarn add @medusajs/file-local 
+# #install pluns
+# yarn add @medusajs/file-local 
 
-yarn add medusa-plugin-meilisearch 
+# yarn add medusa-plugin-meilisearch 
 
-yarn install
+# yarn install
 
 cat <<EOF >> /etc/postgresql/pg_hba.conf host all all 0.0.0.0/0 md5 EOF
 cat <<EOF >> /etc/postgresql/postgresql.conf listen_addresses = '*' EOF
 
 
-access the postgres console
-psql postgres
+# access the postgres console
+# psql postgres
 
-#使用 postgres 用户登录
-psql -U yynid
+# #使用 postgres 用户登录
+# psql -U yynid
 
-#postgres=# 是 postgres 提示符 为 postgres 用户设置密码
-postgres=# \password Syw!888888
+# #postgres=# 是 postgres 提示符 为 postgres 用户设置密码
+# postgres=# \password Syw!888888
 
-# create a new database and the newly created user as the owner
-CREATE DATABASE medusa_db OWNER yynid;
+# # create a new database and the newly created user as the owner
+# CREATE DATABASE medusa_db OWNER yynid;
 
-# grant all privileges on medusa_db to the medusa_admin user
-GRANT ALL PRIVILEGES ON DATABASE medusa_db TO yynid;
+# # grant all privileges on medusa_db to the medusa_admin user
+# GRANT ALL PRIVILEGES ON DATABASE medusa_db TO yynid;
 
-# exit the postgres console
-quit
+# # exit the postgres console
+# quit
 
