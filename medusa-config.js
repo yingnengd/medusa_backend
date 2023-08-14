4afc7e93b2fd59514a43d1814b6ebd81b1134af3
yingnengd <80525738+yingnengd@users.noreply.github.com>
2023-08-14 21:54:34 +0800

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -97,15 +97,15 @@ const plugins = [
 
 module.exports = {
   projectConfig: {
-    // redis_url: REDIS_URL,
-    // For more production-like environment install PostgresQL
-    database_url: DATABASE_URL,
-    // database_type: "postgres",
-    database_database: "./medusa-db.sql",
-    database_type: "sqlite",
+    redis_url: REDIS_URL,
+    For more production-like environment install PostgresQL
+    database_url: DATABASE_URL
+    database_type: "postgres",
+    // database_database: "./medusa-db.sql",
+    // database_type: "sqlite",
     store_cors: STORE_CORS,
     admin_cors: ADMIN_CORS,
-    database_extra: { ssl: { rejectUnauthorized: false } },
+    // database_extra: { ssl: { rejectUnauthorized: false } },
   },
   plugins,
 };
