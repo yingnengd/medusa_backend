eb782a5ff99eb631b41813b5e7f28258a88fbfa5
yingnengd <80525738+yingnengd@users.noreply.github.com>
2023-08-15 09:28:29 +0800

    Update develop.sh

--- develop.sh
+++ develop.sh
@@ -14,16 +14,6 @@ medusa migrations run
 #Start development environment
 medusa develop
 
-# #add user
-# medusa user -e mei@outlook.com -p Syw!888888
-
-# #install pluns
-# yarn add @medusajs/file-local 
-
-# yarn add medusa-plugin-meilisearch 
-
-# yarn install
-
 cat <<EOF >> /etc/postgresql/pg_hba.conf host all all 0.0.0.0/0 md5 EOF
 cat <<EOF >> /etc/postgresql/postgresql.conf listen_addresses = '*' EOF
 
